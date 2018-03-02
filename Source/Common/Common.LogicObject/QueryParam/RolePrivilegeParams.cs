f5ed5b7aa9a15c8b0e2e2dd7c7307a59eb6d8b10
Lozen Lin <lozenlin@gmail.com>
2018-03-02 17:51:40 +0800

    remove Common.DataAccess.RoleOpDescParamsDA;

--- Source/Common/Common.LogicObject/QueryParam/RolePrivilegeParams.cs
+++ Source/Common/Common.LogicObject/QueryParam/RolePrivilegeParams.cs
@@ -1,5 +1,4 @@
-﻿using Common.DataAccess;
-using Common.DataAccess.EF.Model;
+﻿using Common.DataAccess.EF.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -53,41 +52,6 @@ namespace Common.LogicObject
 
 
             return empRoleOps;
-        }
-
-        public List<RoleOpDescParamsDA> GetRoleOpsOfDA()
-        {
-            List<RoleOpDescParamsDA> roleOps;
-
-            roleOps = pvgChanges.Select(c =>
-            {
-                RoleOpDescParamsDA roleOp = new RoleOpDescParamsDA()
-                {
-                    RoleName = c.RoleName,
-                    OpId = c.OpId,
-                    PostAccount = this.PostAccount
-                };
-
-                roleOp.CanRead = (c.PvgOfItem & 1) == 1;
-                roleOp.CanEdit = (c.PvgOfItem & 2) == 2;
-
-                roleOp.CanReadSubItemOfSelf = (c.PvgOfSubitemSelf & 1) == 1;
-                roleOp.CanEditSubItemOfSelf = (c.PvgOfSubitemSelf & 2) == 2;
-                roleOp.CanAddSubItemOfSelf = (c.PvgOfSubitemSelf & 4) == 4;
-                roleOp.CanDelSubItemOfSelf = (c.PvgOfSubitemSelf & 8) == 8;
-
-                roleOp.CanReadSubItemOfCrew = (c.PvgOfSubitemCrew & 1) == 1;
-                roleOp.CanEditSubItemOfCrew = (c.PvgOfSubitemCrew & 2) == 2;
-                roleOp.CanDelSubItemOfCrew = (c.PvgOfSubitemCrew & 8) == 8;
-
-                roleOp.CanReadSubItemOfOthers = (c.PvgOfSubitemOthers & 1) == 1;
-                roleOp.CanEditSubItemOfOthers = (c.PvgOfSubitemOthers & 2) == 2;
-                roleOp.CanDelSubItemOfOthers = (c.PvgOfSubitemOthers & 8) == 8;
-
-                return roleOp;
-            }).ToList();
-
-            return roleOps;
         }
     }
 }
